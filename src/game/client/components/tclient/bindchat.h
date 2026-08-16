d8cd3c32dfc578da0f2a2b86f16d894146b76edb
What962 <artcheverda@gmail.com>
2026-08-17 01:18:15 +0300

    Add BKW DDNet player info command state

--- src/game/client/components/tclient/bindchat.h
+++ src/game/client/components/tclient/bindchat.h
@@ -6,13 +6,21 @@
 #include <game/client/component.h>
 #include <game/client/lineinput.h>
 
+#include <memory>
+#include <string>
+
 class IConfigManager;
+class IHttpRequest;
+struct _json_value;
+typedef struct _json_value json_value;
 
 enum
 {
 	BINDCHAT_MAX_NAME = 64,
 	BINDCHAT_MAX_CMD = 1024,
 	BINDCHAT_MAX_BINDS = 256,
+	BKW_PLAYER_INFO_COMMAND_MAX = 64,
+	BKW_PLAYER_INFO_NAME_MAX = 128,
 };
 
 class CBindChat : public CComponent
@@ -42,10 +50,25 @@ private:
 	static void ConRemoveBindchat(IConsole::IResult *pResult, void *pUserData);
 	static void ConRemoveBindchatAll(IConsole::IResult *pResult, void *pUserData);
 	static void ConBindchatDefaults(IConsole::IResult *pResult, void *pUserData);
+	static void ConBkwPlayerInfo(IConsole::IResult *pResult, void *pUserData);
+	static void ConBkwPlayerInfoEnabled(IConsole::IResult *pResult, void *pUserData);
+	static void ConBkwPlayerInfoChatCommand(IConsole::IResult *pResult, void *pUserData);
 
 	static void ConfigSaveCallback(IConfigManager *pConfigManager, void *pUserData);
 
 	void ExecuteBind(const CBind &Bind, const char *pArgs);
+
+	bool m_BkwPlayerInfoEnabled = true;
+	char m_aBkwPlayerInfoChatCommand[BKW_PLAYER_INFO_COMMAND_MAX] = ".player";
+	char m_aBkwPlayerInfoRequestedName[BKW_PLAYER_INFO_NAME_MAX] = "";
+	std::shared_ptr<IHttpRequest> m_pBkwPlayerInfoRequest;
+
+	bool MatchBkwPlayerInfoCommand(const char *pText, const char **ppPlayerName) const;
+	void StartBkwPlayerInfoRequest(const char *pPlayerName);
+	void FinishBkwPlayerInfoRequest();
+	void PrintBkwPlayerInfo(const json_value &Root);
+	void PrintBkwPlayerInfoLine(const char *pText);
+	static std::string BkwUrlEncode(const char *pText);
 
 public:
 	std::vector<CBind> m_vBinds; // TODO use map
@@ -53,6 +76,8 @@ public:
 	CBindChat();
 	int Sizeof() const override { return sizeof(*this); }
 	void OnConsoleInit() override;
+	void OnRender() override;
+	void OnStateChange(int NewState, int OldState) override;
 
 	void AddBind(const CBind &Bind);
 
@@ -64,6 +89,11 @@ public:
 	bool CheckBindChat(const char *pText);
 	bool ChatDoBinds(const char *pText);
 	bool ChatDoAutocomplete(bool ShiftPressed);
+
+	bool BkwPlayerInfoEnabled() const { return m_BkwPlayerInfoEnabled; }
+	const char *BkwPlayerInfoChatCommand() const { return m_aBkwPlayerInfoChatCommand; }
+	void SetBkwPlayerInfoEnabled(bool Enabled);
+	void SetBkwPlayerInfoChatCommand(const char *pCommand);
 };
 
 #endif
