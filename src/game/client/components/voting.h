90e6c3b0f5f0a089f287980ec64070610af3b121
What962 <artcheverda@gmail.com>
2026-08-18 07:38:09 +0300

    BKW: keep TeeFusion input in global stack

--- src/game/client/components/voting.h
+++ src/game/client/components/voting.h
@@ -3,11 +3,7 @@
 #ifndef GAME_CLIENT_COMPONENTS_VOTING_H
 #define GAME_CLIENT_COMPONENTS_VOTING_H
 
-#include <base/str.h>
-
-#include <engine/client.h>
 #include <engine/console.h>
-#include <engine/keys.h>
 #include <engine/shared/memheap.h>
 
 #include <game/client/component.h>
@@ -50,57 +46,6 @@ public:
 	void OnReset() override;
 	void OnConsoleInit() override;
 	void OnMessage(int MsgType, void *pRawMsg) override;
-	bool OnInput(const IInput::CEvent &Event) override
-	{
-		if(Event.m_Key != KEY_F6 || !(Event.m_Flags & IInput::FLAG_PRESS) || (Event.m_Flags & IInput::FLAG_REPEAT))
-			return false;
-
-		Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "tf-vote", "===== TeeFusion vote options dump =====");
-
-		char aUserName[64] = "";
-		int Index = 0;
-		for(const CVoteOptionClient *pOption = m_pFirst; pOption; pOption = pOption->m_pNext, ++Index)
-		{
-			char aLine[VOTE_DESC_LENGTH + 32];
-			str_format(aLine, sizeof(aLine), "[%d] %s", Index, pOption->m_aDescription);
-			Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "tf-vote", aLine);
-
-			if(aUserName[0] == '\0')
-			{
-				const char *pLabel = str_utf8_find_nocase(pOption->m_aDescription, "Имя пользователя");
-				if(!pLabel)
-					pLabel = str_utf8_find_nocase(pOption->m_aDescription, "Username");
-				if(pLabel)
-				{
-					const char *pValue = str_find(pLabel, ":");
-					if(pValue)
-					{
-						++pValue;
-						while(*pValue == ' ' || *pValue == '\t' || *pValue == '-' || *pValue == '|')
-							++pValue;
-						if(*pValue != '\0')
-						{
-							str_copy(aUserName, pValue, sizeof(aUserName));
-							int Length = str_length(aUserName);
-							while(Length > 0 && (aUserName[Length - 1] == ' ' || aUserName[Length - 1] == '\t' || aUserName[Length - 1] == '\r' || aUserName[Length - 1] == '\n' || aUserName[Length - 1] == '|'))
-								aUserName[--Length] = '\0';
-						}
-					}
-				}
-			}
-		}
-
-		char aSummary[256];
-		if(aUserName[0] != '\0')
-			str_format(aSummary, sizeof(aSummary), "Получено vote options: %d. Имя пользователя: %s", m_NumVoteOptions, aUserName);
-		else
-			str_format(aSummary, sizeof(aSummary), "Получено vote options: %d. Строка имени не найдена. Откройте локальную консоль для полного списка.", m_NumVoteOptions);
-
-		Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "tf-vote", aSummary);
-		Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "tf-vote", m_ReceivingOptions ? "Сервер еще отправляет группу vote options." : "Группа vote options сейчас завершена.");
-		Client()->Notify("TeeFusion vote parser", aSummary);
-		return true;
-	}
 
 	void Render(bool ForcePreview = false);
 	CUIRect GetHudRect(float HudWidth, float HudHeight, bool ForcePreview = false) const;
