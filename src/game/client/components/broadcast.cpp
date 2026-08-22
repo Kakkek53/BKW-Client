19a314ba48e2229e74824ce77f2595dd210be72b
Kakkek53 <artcheverda@gmail.com>
2026-08-22 06:55:32 +0300

    Finalize robust TeeFusion duel parser

--- src/game/client/components/broadcast.cpp
+++ src/game/client/components/broadcast.cpp
@@ -1,3 +1,3 @@
-#include <game/client/components/broadcast_part1.inc>
-#include <game/client/components/broadcast_part2.inc>
+#include <game/client/components/broadcast_part1_v2.inc>
+#include <game/client/components/broadcast_part2_v2.inc>
 #include <game/client/components/broadcast_part3.inc>
