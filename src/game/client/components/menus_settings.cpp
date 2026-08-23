5bd44de367acd1b908ff109daa659ed902948eff
Kakkek53 <artcheverda@gmail.com>
2026-08-23 15:55:50 +0300

    Wire video map converter dependencies into BKW settings

--- src/game/client/components/menus_settings.cpp
+++ src/game/client/components/menus_settings.cpp
@@ -1,2 +1,11 @@
+#include <engine/shared/datafile.h>
+#include <engine/storage.h>
+
+#include <game/client/components/media_decoder.h>
+#include <game/mapitems.h>
+
+#include <array>
+#include <vector>
+
 #include "menus_settings_bkw_part1.inc"
 #include "menus_settings_bkw_part2.inc"
