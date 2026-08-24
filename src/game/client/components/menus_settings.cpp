0915f069ecc7d9c9ed576264e3e5b4e2dc88d893
Kakkek53 <artcheverda@gmail.com>
2026-08-24 15:28:42 +0300

    Fix MP4 map converter compile dependencies

--- src/game/client/components/menus_settings.cpp
+++ src/game/client/components/menus_settings.cpp
@@ -1,12 +1,19 @@
 #include <base/fs.h>
+#include <base/math.h>
+#include <base/mem.h>
+#include <base/str.h>
+#include <base/system.h>
 
 #include <engine/shared/datafile.h>
 #include <engine/storage.h>
 
 #include <game/client/components/media_decoder.h>
+#include <game/gamecore.h>
 #include <game/mapitems.h>
 
+#include <algorithm>
 #include <array>
+#include <cstdlib>
 #include <iterator>
 #include <vector>
 
